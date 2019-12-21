43e3d2032a18260661eb0f93b57f57260f233292
tdorosh <tdorosh4@gmail.com>
2019-12-21 15:27:15 +0200

    add filtersets classes for all objects

--- accounting/filters.py
+++ accounting/filters.py
@@ -10,4 +10,30 @@ class TransactionFilterSet(filters.FilterSet):
     class Meta:
         model = models.Transaction
         fields = ['from_amount', 'to_amount', 'currency', 'trans_type', 'category',
-                'subcategory', 'on_account', 'from_account', 'place']
+                'subcategory', 'on_account', 'from_account', 'create_datetime', 'place', 'notes']
+
+class AccountFilterSet(filters.FilterSet):
+    from_amount = filters.NumberFilter(field_name="amount", lookup_expr='gte')
+    to_amount = filters.NumberFilter(field_name="amount", lookup_expr='lte')
+
+    class Meta:
+        model = models.Account
+        fields = ['title', 'from_amount', 'to_amount', 'currency', 'notes', 'create_datetime']
+
+class AccountJournalFilterSet(filters.FilterSet):
+    from_amount = filters.NumberFilter(field_name="amount_after", lookup_expr='gte')
+    to_amount = filters.NumberFilter(field_name="amount_after", lookup_expr='lte')
+
+    class Meta:
+        model = models.AccountJournal
+        fields = ['account', 'from_amount', 'to_amount', 'timestamp']
+
+class CategoryFilterSet(filters.FilterSet):
+    class Meta:
+        model = models.Category
+        fields = ['name', 'cat_type']
+
+class SubcategoryFilterSet(filters.FilterSet):
+    class Meta:
+        model = models.Subcategory
+        fields = ['name', 'category']
