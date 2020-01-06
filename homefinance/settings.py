c86a16a886d1ba8c5cad0889fd10b20c1b9798e5
tdorosh <tdorosh4@gmail.com>
2020-01-06 19:33:29 +0200

    add changes to cors whitelist

--- homefinance/settings.py
+++ homefinance/settings.py
@@ -152,7 +152,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
     '127.0.0.1:8080',
-    'django-vue-homefinance.herokuapp.com'
+    'django-vue-homefinance.herokuapp.com',
 )
 
 
