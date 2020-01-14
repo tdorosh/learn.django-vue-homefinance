abd3188ac4046b300bca3f86635391d0c213ac29
tdorosh <tdorosh4@gmail.com>
2020-01-14 15:22:44 +0200

    create filter for date field and add filter for amount after

--- accounting/filters.py
+++ accounting/filters.py
@@ -6,27 +6,32 @@ from accounting import models
 class TransactionFilterSet(filters.FilterSet):
     from_amount = filters.NumberFilter(field_name="amount", lookup_expr='gte')
     to_amount = filters.NumberFilter(field_name="amount", lookup_expr='lte')
+    create_date = filters.DateFilter(field_name="create_datetime", lookup_expr='date', input_formats=['%d.%m.%Y'])
 
     class Meta:
         model = models.Transaction
         fields = ['from_amount', 'to_amount', 'currency', 'trans_type', 'category',
-                'subcategory', 'on_account', 'from_account', 'create_datetime', 'place', 'notes']
+                'subcategory', 'on_account', 'from_account', 'create_date', 'place']
 
 class AccountFilterSet(filters.FilterSet):
     from_amount = filters.NumberFilter(field_name="amount", lookup_expr='gte')
     to_amount = filters.NumberFilter(field_name="amount", lookup_expr='lte')
+    create_date = filters.DateFilter(field_name="create_datetime", lookup_expr='date', input_formats=['%d.%m.%Y'])
 
     class Meta:
         model = models.Account
-        fields = ['title', 'from_amount', 'to_amount', 'currency', 'notes', 'create_datetime']
+        fields = ['title', 'from_amount', 'to_amount', 'currency', 'notes', 'create_date']
 
 class AccountJournalFilterSet(filters.FilterSet):
-    from_amount = filters.NumberFilter(field_name="amount_after", lookup_expr='gte')
-    to_amount = filters.NumberFilter(field_name="amount_after", lookup_expr='lte')
+    from_amount_before = filters.NumberFilter(field_name="amount_before", lookup_expr='gte')
+    to_amount_before = filters.NumberFilter(field_name="amount_before", lookup_expr='lte')
+    from_amount_after = filters.NumberFilter(field_name="amount_after", lookup_expr='gte')
+    to_amount_after = filters.NumberFilter(field_name="amount_after", lookup_expr='lte')
+    create_date = filters.DateFilter(field_name="timestamp", lookup_expr='date', input_formats=['%d.%m.%Y'])
 
     class Meta:
         model = models.AccountJournal
-        fields = ['account', 'from_amount', 'to_amount', 'timestamp']
+        fields = ['account', 'from_amount_before', 'to_amount_before', 'from_amount_after', 'to_amount_after', 'create_date']
 
 class CategoryFilterSet(filters.FilterSet):
     class Meta:
